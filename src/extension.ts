e6ddcf956e6a5adaddc66751a2bca34eea3558ee
lyzerk <alm.ozdmr@gmail.com>
2022-01-01 13:17:56 +0300

    vscode configurations, outputchannel and statusbar

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "openapi-client-generator" is now active!');
+	const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+  statusBar.text = "OpenAPI: Initialized";
+  statusBar.show();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+  const output = vscode.window.createOutputChannel("OpenAPI Client Generator");
+  output.appendLine("Initialized")
+  output.show();
+
 	let disposable = vscode.commands.registerCommand('openapi-client-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from openapi-client-generator!');
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(output);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
